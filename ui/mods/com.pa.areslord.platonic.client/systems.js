11ab4ac166719e63f01add8456b3626e7c4557c9
ATLASLORD <104906253+ATLASLORD@users.noreply.github.com>
2023-06-06 23:37:01 +0200

    new system

--- ui/mods/com.pa.areslord.platonic.client/systems.js
+++ ui/mods/com.pa.areslord.platonic.client/systems.js
@@ -2,6 +2,7 @@ cShareSystems.load_pas("Platonic Systems", [
 	"coui://ui/mods/com.pa.areslord.platonic.client/systems/triangle_prime_6p.pas",
 	"coui://ui/mods/com.pa.areslord.platonic.client/systems/hexagonal_virus_4v4.pas",
 	"coui://ui/mods/com.pa.areslord.platonic.client/systems/wet_icosahedron_5v5.pas",
+	"coui://ui/mods/com.pa.areslord.platonic.client/systems/football_6_8_10p.pas",
 	"coui://ui/mods/com.pa.areslord.platonic.client/systems/nipple_pools.pas",
 	"coui://ui/mods/com.pa.areslord.platonic.client/systems/eye_rings_big_4-8p.pas"
 ]);
